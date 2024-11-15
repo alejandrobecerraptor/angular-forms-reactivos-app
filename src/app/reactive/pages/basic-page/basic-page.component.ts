7013b4cbbc5c678f4b090f1b5e53230a4451f7db
Alejandro Bece <alejandro.becerra.pro@gmail.com>
2024-11-15 15:32:21 -0500

    re-establecere y establecer valor al formulario

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,34 +1,45 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+const rtx5090 = {
+  name: 'rtx5090',
+  price: 1000,
+  inStorage: 50,
+}
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: ``,
 })
-export class BasicPageComponent {
 
-
+export class BasicPageComponent implements OnInit {
   // myForm: FormGroup = new FormGroup({
   //   name: new FormControl(''),
   //   price: new FormControl(0),
   //   inStorage: new FormControl(0),
   // });
 
-
   myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
-    price: [0 , [Validators.required, Validators.min(0)]],
-    inStorage: [0 , [Validators.required, Validators.min(0)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
   });
 
   constructor(private fb: FormBuilder) {}
 
-  onSave(): void {
-
-    if (this.myForm.valid) {
-      console.log(this.myForm.value);
-    }
-
+  ngOnInit(): void {
+    this.myForm.reset(rtx5090);
   }
 
+  onSave(): void {
+    if (this.myForm.valid) {
+      console.log(this.myForm.value);
+      this.myForm.reset({ price: 0, inStorage: 0 });
+    }
+  }
 }
