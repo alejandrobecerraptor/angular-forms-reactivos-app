f3e5581af949529052793c27256e442f7d4f8933
Alejandro Bece <alejandro.becerra.pro@gmail.com>
2024-11-17 14:29:16 -0500

    elaborar el validador del correo

--- src/app/shared/validators/email-validator.service.ts
+++ src/app/shared/validators/email-validator.service.ts
@@ -8,13 +8,34 @@ import { delay, Observable, of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class EmailValidator implements AsyncValidator {
+  //otra forma
+  // validate(control: AbstractControl): Observable<ValidationErrors | null> {
+  //   const email = control.value;
+  //   console.log({ email });
+  //   return of({
+  //     emailTaken: true,
+  //   }).pipe(
+  //     delay(2000)
+  //   );
+  // }
+
   validate(control: AbstractControl): Observable<ValidationErrors | null> {
     const email = control.value;
-    console.log({ email });
-    return of({
-      emailTaken: true,
-    }).pipe(
-      delay(2000)
-    );
+    const httpCallObservable = new Observable<ValidationErrors | null>(
+      (subscriber) => {
+        console.log({ email });
+        if (email === 'strider@gmail.com') {
+          subscriber.next({
+            emailTaken: true,
+          });
+          subscriber.complete();
+        }
+
+        subscriber.next(null);
+        subscriber.complete();
+      }
+    ).pipe(delay(2000));
+
+    return httpCallObservable;
   }
 }
